222322f6e07ded6378590b7ecb1f59b4c49730f9
philippe <philippe.frager@wanadoo.fr>
2025-01-25 10:38:19 +0100

    Depot

---
+++ src/Controller/AnimalController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AnimalController extends AbstractController
+{
+    #[Route('/animal', name: 'animal.index')]
+    public function index(Request $request): Response
+    {
+        return new Response('Animaux');
+    }
+
+    #[Route('/animal/{slug}-{id}', name: 'animal.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+      return new Response('Animal : ' . $slug);
+    }
+}
